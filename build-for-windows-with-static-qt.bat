9ada259ad69947b09cb63ec954a2c4962d792034
Jarle Aase <jarle@jgaa.com>
2025-04-19 09:40:51 +0300

    Stepping trough build errors

--- build-for-windows-with-static-qt.bat
+++ build-for-windows-with-static-qt.bat
@@ -24,6 +24,14 @@ if not defined VCPKG_DEFAULT_TRIPLET (
     set "VCPKG_DEFAULT_TRIPLET=x64-windows-release"
 )
 
+echo Preparing to build qtstuff...
+echo SOURCE_DIR is: %SOURCE_DIR%    
+echo BUILD_DIR is: %BUILD_DIR%
+echo QT_TARGET_DIR is: %QT_TARGET_DIR%
+echo VCPKG_ROOT is: %VCPKG_ROOT%
+echo VCPKG_DEFAULT_TRIPLET is: %VCPKG_DEFAULT_TRIPLET%
+
+
 echo Static Qt target dir is: %QT_TARGET_DIR%
 if not exist "%QT_TARGET_DIR%\" (
     echo Qt static build not found. Running build-static-qt.bat...
@@ -48,8 +56,9 @@ if errorlevel 1 (
 
 set "MY_BUILD_DIR=%BUILD_DIR%\qtstuff"
 set TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+echo MY_BUILD_DIR is: %MY_BUILD_DIR%
+echo TOOLCHAIN_FILE is: %TOOLCHAIN_FILE%
 
-echo Path is: %PATH%
 echo Building qtstuff in %MY_BUILD_DIR%
 
 rmdir /S /Q "%MY_BUILD_DIR%"
@@ -59,21 +68,38 @@ if errorlevel 1 (
     exit /b
 )
 
-cmake -S "%SOURCE_DIR%" -B "%MY_BUILD_DIR%" ^
-    -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" ^
-    -DVCPKG_TARGET_TRIPLET=%VCPKG_DEFAULT_TRIPLET% ^
-    -DENABLE_GRPC=ON ^
-    -DCMAKE_PREFIX_PATH="%QT_TARGET_DIR%" ^
-    -DQT_QMAKE_EXECUTABLE="%QT_TARGET_DIR%\bin\qmake" ^
-    -G "Ninja" ^
-    -DCMAKE_BUILD_TYPE=Release
+pushd "%MY_BUILD_DIR%"
 if errorlevel 1 (
-    echo Failed to create run cmake
+    echo Failed to pushd into %MY_BUILD_DIR%
     exit /b
 )
 
+echo %PATH% | find /I "%VCPKG_ROOT%" >nul
+if errorlevel 1 (
+    set "PATH=%VCPKG_ROOT%;%PATH%"
+)
+echo Path is: %PATH%
 
-pushd "%MY_BUILD_DIR%" || exit /b
+echo Running vcpkg install for qtstuff
+vcpkg install --triplet "%VCPKG_DEFAULT_TRIPLET%"
+
+echo Listing vcpkg packages
+vcpkg list
+
+echo "Calling cmake for qtstuff"
+cmake --trace-expand --debug-output ^
+    -S "%SOURCE_DIR%" -B "%MY_BUILD_DIR%" ^
+    -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" ^
+    -DVCPKG_TARGET_TRIPLET="%VCPKG_DEFAULT_TRIPLET%" ^
+    -DENABLE_GRPC=ON ^
+    -DCMAKE_PREFIX_PATH="%QT_TARGET_DIR%" ^
+    -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE=Release
+if errorlevel 1 (
+    echo Failed to run cmake
+    exit /b
+)
+
 set "PATH=%MY_BUILD_DIR%\vcpkg_installed\%VCPKG_DEFAULT_TRIPLET%\tools\brotli;%MY_BUILD_DIR%\vcpkg_installed\%VCPKG_DEFAULT_TRIPLET%\bin;%MY_BUILD_DIR%\bin;%PATH%"
 
 cmake --build . --config Release
@@ -82,7 +108,6 @@ if errorlevel 1 (
     exit /b
 )
 
-rem cmake --install . --config Release
 cpack -G NSIS
 
 copy /Y "%MY_BUILD_DIR%\*.exe" "%BUILD_DIR%\"
